3bf1d8b433bd439d9b2a4db52f36c5e5f4635368
Gabriel Vasconcelos <gabriel.vasconcelos@picpay.com>
2020-07-23 17:28:05 -0300

    Cleans database class

--- data/src/main/java/com/gabrielfv/pay/data/AppDatabase.kt
+++ data/src/main/java/com/gabrielfv/pay/data/AppDatabase.kt
@@ -1,8 +1,6 @@
 package com.gabrielfv.pay.data
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.gabrielfv.pay.data.access.UserDao
 import com.gabrielfv.pay.data.entities.User
@@ -15,11 +13,4 @@ import com.gabrielfv.pay.data.entities.User
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
-
-    companion object {
-        fun create(context: Context): AppDatabase = Room.databaseBuilder(
-            context,
-            AppDatabase::class.java, "pay-database"
-        ).build()
-    }
 }
